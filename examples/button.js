bfe915ee7af387858ac30bd96532128f352eba60
Miso Tanaka <miso.develop@gmail.com>
2023-03-01 22:13:00 +0900

    [chore] button example見直し

--- examples/button.js
+++ examples/button.js
@@ -3,29 +3,29 @@ const { Opniz } = require("opniz")
 const port = 3000 // 任意のポートを指定（opnizデバイスの指定と合わせる）
 const opniz = new Opniz.M5Unified({ port }) // opnizインスタンス生成
 
+
+
+// onClickedをオーバーライド
+opniz.Btn.onClicked = (button, count) => console.log(`onClicked: ${JSON.stringify({ button, count })}`)
+
+// onSingleClickedをオーバーライド
+opniz.Btn.onSingleClicked = (button) => console.log(`${button} ボタンが押されました！`)
+
+// onDoubleClickedをオーバーライド
+opniz.Btn.onDoubleClicked = (button) => console.log(`${button} ボタンがダブルクリックされました！`)
+
+// onDeciedClickCountをオーバーライド
+opniz.Btn.onDeciedClickCount = (button, count) => console.log(`${button} ボタンが${count}回連続で押されました！`)
+
+// onHoldをオーバーライド
+opniz.Btn.onHold = (button) => console.log(`${button} ボタンが長押しされました！`)
+
+
+
 const main = async () => {
 	// opnizデバイスへ接続
 	while (!(await opniz.connectWait())) console.log("connect...")
 	console.log("[connected]")
-	
-	
-	
-	// onClickedをオーバーライド
-	opniz.Btn.onClicked = (button, count) => console.log(`onClicked: ${JSON.stringify({ button, count })}`)
-	
-	// onSingleClickedをオーバーライド
-	opniz.Btn.onSingleClicked = (button) => console.log(`${button} ボタンが押されました！`)
-	
-	// onDoubleClickedをオーバーライド
-	opniz.Btn.onDoubleClicked = (button) => console.log(`${button} ボタンがダブルクリックされました！`)
-	
-	// onDeciedClickCountをオーバーライド
-	opniz.Btn.onDeciedClickCount = (button, count) => console.log(`${button} ボタンが${count}回連続で押されました！`)
-	
-	// onHoldをオーバーライド
-	opniz.Btn.onHold = (button) => console.log(`${button} ボタンが長押しされました！`)
-	
-	
 	
 	try {
 		for (;;) {
